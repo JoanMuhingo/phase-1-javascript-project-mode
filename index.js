130e947d59b0941a2506217013abb0919939f85c
Joan <joanmuhingo@gmail.com>
2023-08-13 17:21:02 +0300

    final changes

--- index.js
+++ index.js
@@ -1,90 +1,82 @@
-
+//displaying the entire menu//
 document.addEventListener("DOMContentLoaded", function () {
+    const menuBox = document.getElementById('menu');
     fetch("http://localhost:3000/menu")
         .then(response => response.json())
         .then(data => {
-            const breakfastBox = document.getElementById("breakfast");
-            const firstBreakfast = data.menu.breakfast[0];
-            displayData(breakfastBox, firstBreakfast);
+            const menu = data.menu;
 
-            const lunchBox = document.getElementById("lunch");
-            const firstLunch = data.menu.lunch[0];
-            displayData(lunchBox, firstLunch);
+            for (const category in menu) {
+                const categoryTitle = document.createElement('h2');
+                categoryTitle.textContent = category;
+                menuBox.appendChild(categoryTitle);
 
-            const dinnerBox = document.getElementById("dinner");
-            const firstDinner = data.menu.dinner[0];
-            displayData(dinnerBox, firstDinner);
+                const items = menu[category];
+                const itemList = document.createElement('ul');
 
-            const drinkBox = document.getElementById("drinks");
-            const firstDrink = data.menu.drinks[0];
-            displayData(drinkBox, firstDrink);
-
-            const accompanimentBox = document.getElementById("accompaniments");
-            const firstAccompaniment = data.menu.accompaniments[0];
-            displayData(accompanimentBox, firstAccompaniment);
+                items.forEach(item => {
+                    const listItem = document.createElement('li');
+                    listItem.innerHTML = `
+                        <img src="${item.img}" alt="${item.name}" width="200">
+                        <h2>${item.name}</h2>
+                        <p>${item.description}</p>
+                        <p>Price: $${item.price}</p>
+                        <button onclick="orderItem('${item.name}')">Order</button>
+                    `;
+                    itemList.appendChild(listItem);
+                });
+                menuBox.appendChild(itemList);
+            }
         })
-        .catch(error => {
-            console.error("Error fetching or displaying data:", error);
-        });
+        .catch(error => console.error('Error fetching data:', error));
+
+    addCategoryEventListeners();
 });
-
-function displayData(box, item){
-        const itemElement = document.createElement("div");
-        itemElement.innerHTML = `
-        <img src="${item.img}" alt="${item.name}" width="200">
-        <h2>${item.name}</h2>
-        <p>${item.description}</p>
-        <p>Price: $${item.price}</p>
-        <button onclick="orderItem(${item.name})">Order</button>
-        `;
-        box.appendChild(dataElement);
-    }
-function orderItem(itemName){
-    alert(`your ${itemName}order has been received`)
-};
-displayData();
-orderItem();
-
-function displayMenu(){
-    fetch("http://localhost:3000/menu")
+//displaying menu categories//
+const menuBox = document.getElementById('menu');
+const categoryBox = document.querySelectorAll(`.category`);
+function loadMenus(category) {
+    fetch(`http://localhost:3000/menu/${category}`)
         .then(response => response.json())
-        .then(data =>{
-            const menuBox = document.getElementById("menu-link");
-            const breakfastItems = data.menu.breakfast;
-        displayItems(menuBox, "Breakfast", breakfastItems);
+        .then(data => {
+            const items = data;
+            categoryBox.innerHTML = '';
 
-        const lunchItems = data.menu.lunch;
-        displayItems(menuBox, "lunch", lunchItems);
-
-        const dinnerItems = data.menu.dinner;
-        displayItems(menuBox, "dinner", dinnerItems);
-
-        const drinksItems = data.menu.drinks;
-        displayItems(menuContainer, "drinks", drinksItems);
-
-        const accompanimentsItems = data.menu.accompaniments;
-        displayItems(menuBox, "accompaniments", accompanimentsItems);
-
-
+            items.forEach(item => {
+                const itemElement = document.createElement("div");
+                itemElement.classList.add('menu-item');
+                itemElement.innerHTML = `
+                    <img src="${item.img}" alt="${item.name}" width="200">
+                    <h2>${item.name}</h2>
+                    <p>${item.description}</p>
+                    <p>Price: $${item.price}</p>
+                    <button onclick="orderItem('${item.name}')">Order</button>
+                `;
+                categoryBox.appendChild(itemElement);
+            });
         })
-
+        .catch(error => console.error('Error fetching data:', error));
 }
-function displayItems(box, section, items) {
-    const sectionElement = document.createElement("h2");
-    sectionElement.textContent = section;
-    box.appendChild(sectionElement);
 
-    items.forEach(item => {
-        const itemElement = document.createElement("div");
-        itemElement.innerHTML = `
-        img src="${item.img}" alt="${item.name}" width="200">
-        <h2>${item.name}</h2>
-        <p>${item.description}</p>
-        <p>Price: $${item.price}</p>
-        <button onclick="orderItem(${item.name})">Order</button>
-        `;
-        box.appendChild(itemElement);
+function addCategoryEventListeners() {
+    categoryBox.forEach(category => {
+        category.addEventListener('click', function () {
+            const category = this.getAttribute('data-category');
+            loadMenus(category);
+        });
     });
-}
+};
+//alert when order is made//
+function orderItem(itemName, itemPrice) {
+    alert(`You ordered ${itemName} for $${itemPrice} has been received`);
+};
 
-displayMenu();
+
+
+
+
+
+
+
+
+           
